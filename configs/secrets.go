52d85605c665d6e98c680b8fa652e0039dba9766
Vinod Thorat <thoratvinod111@gmail.com>
2024-08-07 13:58:56 +0530

    Remove unwanted file

--- configs/secrets.go
+++
@@ -1,7 +0,0 @@
-package configs
-
-const (
-	PublishableTestKey = "pk_test_51PhT0rLDOmWPE4uMluA4kd9gJ3q3qpOkgTHtCYMVFHdLbbgqSxp2tOwJLCtqRoXxagsepXPW3BL1mIAR8Jbuw7rb006JZ8AA0w"
-	// SecretTestKey      = "sk_test_51PhT0rLDOmWPE4uMOe95LkIZQxE3e7j0u3cWhTWLzWwwdqKIAvWohc64lYaVZLVjbr1NFUkW53JkQAW275B2uTKc00WLK5pozF"
-	SecretTestKey = "this-is-secret-key"
-)
